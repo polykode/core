55161064496f28195e2cacaf028c9b09d149dd6a
Akshay Nair <phenax5@gmail.com>
2021-02-04 18:10:21 +0530

    Cleanup for lxd build system

--- default.nix
+++ default.nix
@@ -1,1 +1,12 @@
-shells/dev.nix
+{ nixpkgs ? import <nixpkgs> {}, haskellPackages ? nixpkgs.haskellPackages }:
+let
+  inherit (nixpkgs) pkgs;
+  systemPackages = with pkgs; [
+    haskellPackages.haskell-language-server
+    haskellPackages.cabal-install
+    lxd
+  ];
+in pkgs.stdenv.mkDerivation {
+  name = "lxc-env";
+  buildInputs = systemPackages;
+}
