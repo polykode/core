51d666152ca1838d218d0e7146d70203183d3b11
Akshay Nair <phenax5@gmail.com>
2021-02-08 21:37:24 +0530

    Fixes bash template for context api typo

--- src/CodeExecutor/Langs/Bash/Templates.hs
+++ src/CodeExecutor/Langs/Bash/Templates.hs
@@ -16,8 +16,8 @@ context() {
     ++ var "baseUrl" serverBaseUrl
     ++ [r|
   case "$#" in
-    1) curl "${baseUrl}/uffi/var/$1?exec_id=${execId}" 2>/dev/null | jq -r ".value" ;;
-    2) curl -X POST "${baseUrl}/uffi/var/$1?exec_id=${execId}" -d "value=$2" >/dev/null 2>&1 ;;
+    1) curl "${baseUrl}/uffi/variable/$1?exec_id=${execId}" 2>/dev/null | jq -r ".value" ;;
+    2) curl -X POST "${baseUrl}/uffi/variable/$1?exec_id=${execId}" -d "value=$2" >/dev/null 2>&1 ;;
     *) echo "Invalid set of arguments passed to 'context'";
   esac
 }
