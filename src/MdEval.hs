bdf9b092de6ce2314174f3eb1532638a514b5105
Akshay Nair <phenax5@gmail.com>
2021-02-08 22:10:00 +0530

    Adds to json conversion for resultnode

--- src/MdEval.hs
+++ src/MdEval.hs
@@ -6,8 +6,10 @@ import CMark
 import CodeExecutor
 import Container.Eff
 import Control.Algebra
-import qualified Data.Text as T
+import qualified Data.Aeson as Json
+import qualified Data.Text as Text
 import Parser
+import Server.JsonResponse
 
 type EvaluationResult = Result
 
@@ -19,7 +21,27 @@ data ResultNode
 instance Show ResultNode where
   show (RenderNode (Node _ nodeType cNodes) cResults) =
     show nodeType ++ ": [ " ++ show cResults ++ "] " -- ++ "(" ++ show rest ++ ")"
-  show (EvalNode node result) = show node ++ "\n\n--- Result ---\n" ++ show result
+  show (EvalNode node result) = show node
+
+instance Json.ToJSON ResultNode where
+  toJSON (RenderNode node children) =
+    Json.object
+      [ (Text.pack "type", toJsonString "render"),
+        -- (Text.pack "node", toJsonString . show $ node),
+        (Text.pack "children", Json.toJSONList . map Json.toJSON $ children)
+      ]
+  toJSON (EvalNode node (exitCode, stdout, stderr)) =
+    Json.object
+      [ (Text.pack "type", toJsonString "eval"),
+        (Text.pack "node", toJsonString . show $ node),
+        ( Text.pack "result",
+          Json.object
+            [ (Text.pack "exitCode", toJsonString . show $ exitCode),
+              (Text.pack "stdout", toJsonString stdout),
+              (Text.pack "stderr", toJsonString stderr)
+            ]
+        )
+      ]
 
 evaluateMd :: Has LxcIOErr sig m => String -> Container -> [XMDNode] -> m [ResultNode]
 evaluateMd execId container [] = return []
