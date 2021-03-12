481b2f624d9004acf886d14cab1f2dfd26f4e69a
Akshay Nair <phenax5@gmail.com>
2021-03-13 00:58:03 +0530

    nix change

--- default.nix
+++ default.nix
@@ -4,11 +4,11 @@ let
   systemPackages = with pkgs; [
     haskellPackages.haskell-language-server
     haskellPackages.cabal-install
-    haskellPackages.hsc2hs
     lxd
     zlib
   ];
-in pkgs.stdenv.mkDerivation {
+in
+pkgs.stdenv.mkDerivation {
   name = "lxc-env";
   buildInputs = systemPackages;
 }
